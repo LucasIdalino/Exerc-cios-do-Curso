48425e0e2bc46cd878cad6485b66c5f6fdcbfaf3
Lucas Idalino <93747485+LucasIdalino@users.noreply.github.com>
2022-01-13 17:38:06 -0300

    Verificando as primeiras letras de um texto.

---
+++ ex024.py
@@ -0,0 +1,7 @@
+
+#A função .strip() retira os espaços do ínicio e fim da palavra
+cidade = str(input("Qual a cidade? ")).strip()
+
+
+#A função .upper() transfroma as letras da palavra em maiúscula.
+print(cidade[:5].upper() == "SANTO")
