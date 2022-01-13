38c6d2b72d9943a6a9f575ab4dcd3b7bbaac2748
Lucas Idalino <93747485+LucasIdalino@users.noreply.github.com>
2022-01-13 16:20:38 -0300

    Tabuada de multiplicação.

---
+++ ex009.py
@@ -0,0 +1,16 @@
+n = int(input("\033[4;34mDigite um número para saber toda tabuada de multiplicação do número digitado\033[m: "))
+
+print("Tabuada de {}".format(n))
+
+#Tabuada do valor da variável.
+
+print("\033[1;31m{}\033[m*\033[1;31m{}\033[m = \033[1;33m{}\033[m".format(n, 1, (n*1)))
+print("\033[1;31m{}\033[m*\033[1;31m{}\033[m = \033[1;33m{}\033[m".format(n, 2, (n*2)))
+print("\033[1;31m{}\033[m*\033[1;31m{}\033[m = \033[1;33m{}\033[m".format(n, 3, (n*3)))
+print("\033[1;31m{}\033[m*\033[1;31m{}\033[m = \033[1;33m{}\033[m".format(n, 4, (n*4)))
+print("\033[1;31m{}\033[m*\033[1;31m{}\033[m = \033[1;33m{}\033[m".format(n, 5, (n*5)))
+print("\033[1;31m{}\033[m*\033[1;31m{}\033[m = \033[1;33m{}\033[m".format(n, 6, (n*6)))
+print("\033[1;31m{}\033[m*\033[1;31m{}\033[m = \033[1;33m{}\033[m".format(n, 7, (n*7)))
+print("\033[1;31m{}\033[m*\033[1;31m{}\033[m = \033[1;33m{}\033[m".format(n, 8, (n*8)))
+print("\033[1;31m{}\033[m*\033[1;31m{}\033[m = \033[1;33m{}\033[m".format(n, 9, (n*9)))
+print("\033[1;31m{}\033[m*\033[1;31m{}\033[m = \033[1;33m{}\033[m".format(n, 10, (n*10)))
