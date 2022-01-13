2446634d2fd05b798a3d79a339abc4138147f4b6
Lucas Idalino <93747485+LucasIdalino@users.noreply.github.com>
2022-01-13 15:58:05 -0300

    Número antecessor e sucessor da variável.

---
+++ ex005.py
@@ -0,0 +1,4 @@
+#Número antecessor e sucessor da variável.
+
+x = int(input("Número: "))
+print("O antecessor de \033[1;33m{}\033[m é \033[1;34m{}\033[m e o sucessor é \033[1;32m{}\033[m!".format(x, (x-1), (x+1)))
